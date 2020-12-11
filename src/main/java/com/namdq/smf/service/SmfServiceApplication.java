2c442ba687850bc895034931ed25deee7e90cb30
namdq <namdq@fintech.smartosc.com>
2020-12-11 16:33:55 +0700

    init 4

---
+++ src/main/java/com/namdq/smf/service/SmfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.namdq.smf.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmfServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmfServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namdq/smf/service/SmfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namdq.smf.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmfServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
